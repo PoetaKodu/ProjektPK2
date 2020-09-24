d8cf07a4b60c38fb6df3fd94202b0d3058d57ffc
PoetaKodu <rfpawelsyska@gmail.com>
2020-09-25 01:47:57 +0200

    Fixed missing parameter.

--- src/Filter.c
+++ src/Filter.c
@@ -16,14 +16,14 @@ bool filterInputFile(char const* inFilePath, char const* outFilePath, PrefixTree
 	String word = bfrReadUntilWs(reader);
 	while(word.len > 0)
 	{
-		String prefix;
-		bool filterResult = prefixFilter(prefixTree, word, &prefix);
+		// String prefix;
+		bool filterResult = prefixFilter(prefixTree, word);
 
 		printf("(%c) Word: %.*s", filterResult ? '+' : '-', word.len, word.data);
-		if (filterResult)
-			printf(", prefix: \"%.*s\"\n", prefix.len, prefix.data);
-		else
-			putchar('\n');
+		// if (filterResult)
+		// 	printf(", prefix: \"%.*s\"\n", prefix.len, prefix.data);
+		// else
+		putchar('\n');
 
 		destroyString(&word);
 		word = bfrReadUntilWs(reader);
